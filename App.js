1ac4d0cacd71503cf466ffd4a5ab3f1297e0fb35
Welliton Araujo <wellitonaraujodev@hotmail.com>
2022-03-05 13:21:04 -0300

    projeto iniciado

---
+++ App.js
@@ -0,0 +1,44 @@
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+
+import logo from './src/assets/logo.png';
+
+
+
+export default function App () {
+
+  return (
+    <SafeAreaView style={styles.backgroundStyle}>
+        <Image 
+          style={styles.logoImg}
+          source={logo}/>
+        <Text style={styles.title}>Olá, Welliton</Text>
+        <Text>Encontre os melhores produtores</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: '#f6f6f6',
+    padding: 16,
+  },
+  logoImg: {
+    width:70,
+    height: 28,
+  },
+  title: {
+    marginTop: 24,
+    fontSize: 26,
+    lineHeight: 26,
+  }
+});
+
